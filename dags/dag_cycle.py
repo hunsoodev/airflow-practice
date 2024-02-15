bc38ebc1e68ac25605e96ea6c2ba4b10531a5f8b
Jung Hun Soo <hunsoodev@gmail.com>
2024-02-15 23:22:11 +0900

    test

--- dags/dag_cycle.py
+++ dags/dag_cycle.py
@@ -11,6 +11,6 @@ dag = DAG(
 t1 = DummyOperator(task_id="t1", dag=dag)
 t2 = DummyOperator(task_id="t2", dag=dag)
 t3 = DummyOperator(task_id="t3", dag=dag)
-t4 = DummyOperator(task_id="t3", dag=dag)
+t4 = DummyOperator(task_id="t4", dag=dag)
 
-t1 >> t2 >> t3
+t1 >> t2 >> t3 >> t4
